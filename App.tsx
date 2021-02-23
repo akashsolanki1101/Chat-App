4e27d0cb53e03734137a83c64271fdfd34370655
akashsolanki1101 <akashsolanki2509@gmail.com>
2021-02-24 00:46:37 +0530

    Amplify for backend is added and auth is also added.

--- App.tsx
+++ App.tsx
@@ -3,6 +3,13 @@ import React from 'react';
 import {ThemeManager} from './components/themeManager/themeManager'
 import {Wrapper} from './components/layoutWrapper/wrapper'
 
+
+import { withAuthenticator } from 'aws-amplify-react-native'
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+
+Amplify.configure(config)
+
 const App = ()=>{
   return (
     <ThemeManager>
@@ -11,6 +18,6 @@ const App = ()=>{
   );
 }
 
-export default App
+export default withAuthenticator(App)
 
 
