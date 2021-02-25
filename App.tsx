66660be5419b34fee8bda8c700aba04cefb39990
akashsolanki1101 <akashsolanki2509@gmail.com>
2021-02-25 23:03:07 +0530

    Relation between chatroom and user is implemented..

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React,{useEffect,useCallback} from 'react';
 
 import {ThemeManager} from './components/themeManager/themeManager'
 import {Wrapper} from './components/layoutWrapper/wrapper'
 
+import {
+  Auth,
+  API,
+  graphqlOperation
+} from 'aws-amplify'
+
+import {getUser} from './graphql/queries'
+import {createUser} from './graphql/mutations'
 
 import { withAuthenticator } from 'aws-amplify-react-native'
 import Amplify from 'aws-amplify'
@@ -11,6 +19,53 @@ import config from './aws-exports'
 Amplify.configure(config)
 
 const App = ()=>{
+
+  const fetchUserDetails = useCallback(async ()=>{ //for fetching currently logged in user details
+    try{
+      const userDetails = await Auth.currentAuthenticatedUser({bypassCache:true})
+      return userDetails
+    }catch(err){
+      console.log(err);
+    }
+  },[])
+
+  const createUserInDb = useCallback(async (newUser:object)=>{
+    try{
+      await API.graphql(graphqlOperation(createUser,{input:newUser}))
+    }catch(err){
+      console.log(err);
+    }
+  },[])
+
+  const getUserData = useCallback(async ()=>{ //for fetching currently logged in user data from db
+    const userDetails = await fetchUserDetails()
+    
+    if(userDetails){
+      const userId = userDetails.attributes.sub
+      try{
+        const userData = await API.graphql(graphqlOperation(getUser,{id:userId}))
+        if(userData.data.getUser){
+          console.log(userData);
+        }else{
+          const newUser = {
+            id: userId,
+            name:userDetails.username,
+            imageUri:'',
+            status:"Let's chAt."
+          }
+          await createUserInDb(newUser)
+        }
+      }catch(err){
+        console.log(err);
+      }
+    }
+    
+  },[])
+
+  useEffect(()=>{
+    getUserData()
+  },[getUserData])
+
   return (
     <ThemeManager>
       <Wrapper/>
